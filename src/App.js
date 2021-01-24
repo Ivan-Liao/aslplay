6fd0378d5ebd32715af37627da0b8f66e80529e8
Ivan Liao <ivanhliao@gmail.com>
2021-01-24 12:06:24 -0500

    2021-01-24 12:06

--- src/App.js
+++ src/App.js
@@ -1,53 +1,68 @@
+import React, {useState} from 'react';
 import './App.css';
-import {Row, Col, Nav, Tab} from 'react-bootstrap';
+import {Row, Col, Nav, Tab, Navbar} from 'react-bootstrap';
 import PlayTab from './components/PlayTab.js'
+
+
 
 function Sonnet() {
 	return (
 		<p>
-			test
+			To Be Filled
 		</p>
 	);
 }
 
 function App() {
+  // states
+  const [promptText, setPromptText] = useState("");
+
+  // once on load
+
   return (
-    <Tab.Container defaultActiveKey="first" className="main-tabs-container">
-      <Row>
-        <Col sm={12}>
-          <Nav variant="pills" className="flex-row" >
-            <Nav.Item>
-              <Nav.Link eventKey="first">Home Page</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="second">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="third">Learn</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="fourth">Play</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-        <Col sm={12}>
-          <Tab.Content>
-            <Tab.Pane eventKey="first" className="main-tab">
-              <Sonnet />
-            </Tab.Pane>
-            <Tab.Pane eventKey="second" className="main-tab">
-              <Sonnet />
-            </Tab.Pane>
-            <Tab.Pane eventKey="third" className="main-tab">
-              <Sonnet />
-            </Tab.Pane>
-            <Tab.Pane eventKey="fourth" className="main-tab">
-              <PlayTab />
-            </Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+      <Tab.Container defaultActiveKey="first" className="main-tabs-container">
+        <Row>
+          <Col>
+            <Nav variant="pills" expand="lg">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Home </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">About</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">Learn</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="fourth">Play</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <Sonnet />
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <Sonnet />
+              </Tab.Pane>
+              <Tab.Pane eventKey="third">
+                <Sonnet />
+              </Tab.Pane>
+              <Tab.Pane eventKey="fourth">
+                <PlayTab promptText={ promptText } setPromptText={ setPromptText }/>
+                <div className="circles">
+                  <div className="circle1"></div>
+                  <div className="circle2"></div>
+                  <div className="circle3"></div>
+                </div>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
   );
 }
 
--- src/components/PlayTab.js
+++ src/components/PlayTab.js
@@ -1,19 +1,37 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import {Container, Row, Button} from 'react-bootstrap';
+import Camera from '../camera.png';
 const alphabet = 'ABCDEFGIJKLMNOPQRSTUVWXYZ'.split('');
-var cur_alphabet = '';
 
-function PlayTab() {
-    function getAlphabetHandler() {
-        cur_alphabet = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log(cur_alphabet);
-    }
-    return (
-        <React.Fragment>
-            <div className="play-prompt"> {cur_alphabet} </div>
-            <button onClick={ getAlphabetHandler }>New Prompt</button>
-        </React.Fragment>
-    )
+function PlayTab({promptText, setPromptText}) {
+  // one off useEffects
+  useEffect(() => {
+    getAlphabetHandler();
+  },[])
+
+  function getAlphabetHandler() {
+    setPromptText(alphabet[Math.floor(Math.random() * alphabet.length)]);
+  }
+  return (
+
+    <Container>
+      <Row>
+        <div className="play-prompt">
+          Make the sign for "{promptText}"
+        </div>
+      </Row>
+      <Row>
+        <Button className="camera-button">
+          <img src={Camera} width="300"/></Button>
+      </Row>
+      <Row>
+        <input accept="image/*" id="icon-button-file" type="file" capture="user"/>
+      </Row>
+      <Row>
+        <Button onClick={getAlphabetHandler} className="general-button">New Prompt</Button>
+      </Row>
+    </Container>
+  );
 }
 
 export default PlayTab;
